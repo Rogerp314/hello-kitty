9e960d9f31d5863485a1be7fe0df30fa7ce30d5d
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-06 00:34:36 -0300

    Update de segurança

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let nome = document.querySelector('#name')
 let senha = document.querySelector('#senha')
+let dados = {Nome: 'Gaby', Senha: 'euteamo!'}
 let res = document.querySelector('#resposta')
-let dados = {Nome: nome}
 
-function cu(){
-    res.innerHTML=`${nome.value}`
+function entrar(){
+    res.style.color='black'
+    if(nome.value.length==0 || senha.value.length==0){
+        res.innerHTML='[ERROR] Preencha os dados corretamente!'
+        res.style.color='red'
+        nome.focus()
+    } else if(nome.value != dados.Nome || senha.value != dados.Senha){
+        res.innerHTML='Nome ou senha incorrtos. Verifique-os'
+        res.style.color='red'
+        nome.value=''
+        nome.focus()
+        senha.value=''
+    } else{
+        return location.replace('apresentacao.html')
+    }
 }
